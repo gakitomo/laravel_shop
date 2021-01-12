0c9a9b93eedf74653bffd7fc8de42fb57f90e26d
gakitomo <tmo8002@gmail.com>
2021-01-12 23:00:20 +0900

    Update Item.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('items/{item}', 'ItemsController@showItemDetail')->name('item');
 
 Route::middleware('auth')
     ->group(function(){
+        Route::get('items/{item}/buy', function () { return "商品購入画面";})->name('item.buy');
+
         Route::get('sell', 'SellController@showSellForm')->name('sell');
         Route::post('sell', 'SellController@sellItem')->name('sell');
     });
