a1c87f64c5a2096d69b06df4138cf85e90158c46
gakitomo <tmo8002@gmail.com>
2021-01-12 22:44:41 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('', 'ItemsController@showItems')->name('top');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('items/{item}', function(){return "商品詳細";})->name('item');
+Route::get('items/{item}', 'ItemsController@showItemDetail')->name('item');
 
 Route::middleware('auth')
     ->group(function(){
