1b11eae15ed09a6d87678d9c591b53b93e945f4e
gakitomo <tmo8002@gmail.com>
2021-01-19 21:18:04 +0900

    make it better

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -38,8 +38,7 @@ class ItemsController extends Controller
         }
 
         $items = $query->orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
-            ->orderBy('id', 'DESC')
-            ->paginate(10);
+            ->orderBy('id', 'DESC');
 
         return view('items.items')
             ->with('items', $items);
