4659c67d08f792c2d1087d31322319a760c7a2de
gakitomo <tmo8002@gmail.com>
2021-01-21 16:31:04 +0900

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Payjp\Payjp;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Payjp::setApiKey(config('payjp.secret_key'));
+        Schema::defaultStringLength(191);
     }
 }
